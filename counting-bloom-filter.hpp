f6c403656b32dbf8fb97c9d9bb9855ccce833afa
Professor-Peanut <professorpeanut.unpaid@simplelogin.fr>
2020-09-06 19:27:11 +0800

    dne

--- counting-bloom-filter.hpp
+++ counting-bloom-filter.hpp
@@ -61,7 +61,27 @@ public:
     
     constexpr void remove(Inttype hash)
     {
-        
+        Inttype mask = 0;
+        for (int i=0; i<N_SLOTS; i+=8)
+        {
+            counters[i+0] -= hash & 1; hash >>= 1;
+            counters[i+1] -= hash & 1; hash >>= 1;
+            counters[i+2] -= hash & 1; hash >>= 1;
+            counters[i+3] -= hash & 1; hash >>= 1;
+            counters[i+4] -= hash & 1; hash >>= 1;
+            counters[i+5] -= hash & 1; hash >>= 1;
+            counters[i+6] -= hash & 1; hash >>= 1;
+            counters[i+7] -= hash & 1; hash >>= 1;
+            mask |= !!counters[i+0] << i  ;
+            mask |= !!counters[i+1] << i+1;
+            mask |= !!counters[i+2] << i+2;
+            mask |= !!counters[i+3] << i+3;
+            mask |= !!counters[i+4] << i+4;
+            mask |= !!counters[i+5] << i+5;
+            mask |= !!counters[i+6] << i+6;
+            mask |= !!counters[i+7] << i+7;
+        }
+        bitmap = bitmap & mask;
     }
     
     constexpr bool maybeHave(Inttype hash) const
@@ -76,21 +96,33 @@ constexpr unsigned cbf__test()
 {
     CountingBloomFilter bloomTest;
     
-    bloomTest.put(0x06);
-    const bool TEST_PUT_OK = bloomTest.maybeHave(0x06);
-    const bool TEST_SURE_DOESNT_HAVE = bloomTest.maybeHave(0x01) == false;
-    const bool TEST_OVERLAPPING_BITS = false;
+    bloomTest.put(0b0110);
+    const bool TEST_PUT_OK = bloomTest.maybeHave(0b0110);
+    const bool TEST_SURE_DOESNT_HAVE = bloomTest.maybeHave(0b0001) == false;
+    const bool TEST_OVERLAPPING_BITS = bloomTest.maybeHave(0b0100) == true;
+    const bool TEST_CONFLICTING_BITS = bloomTest.maybeHave(0b1100) == false;
+    bloomTest.put(0b1111);
+    bloomTest.remove(0b0110);
+    const bool TEST_KEY_FOUND_AFTER_REMOVE = bloomTest.maybeHave(0b0110) == true;
+    bloomTest.remove(0b1111);
+    const bool TEST_KEY_NOT_FOUND_AFTER_REMOVE = bloomTest.maybeHave(0b1111) == false;
     
-    return TEST_PUT_OK
+    return  TEST_PUT_OK
          | (TEST_SURE_DOESNT_HAVE << 1)
          | (TEST_OVERLAPPING_BITS << 2)
+         | (TEST_CONFLICTING_BITS << 3)
+         | (TEST_KEY_FOUND_AFTER_REMOVE << 4)
+         | (TEST_KEY_NOT_FOUND_AFTER_REMOVE << 5)
          ;
 }
 static constexpr unsigned resCBF = cbf__test();
 
-static_assert(resCBF& 1, "Didn't work");
-static_assert(resCBF& 2, ".maybeHave() returned 'true' when the key clearly didn't exist");
-static_assert(resCBF& 4, "");
+static_assert(resCBF&  1, "Didn't work");
+static_assert(resCBF&  2, ".maybeHave() returned 'true' when the key clearly didn't exist");
+static_assert(resCBF&  4, ".maybeHave() overlapping bits");
+static_assert(resCBF&  8, ".maybeHave() conflicting bits");
+static_assert(resCBF& 16, "key disappeard after remove");
+static_assert(resCBF& 32, "key should have disappeard after remove");
 
 
 #endif // COUNTING_BLOOM_FILTER_H
